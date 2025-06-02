21a4a004d5a78aa71ac0e361d36c38dce892d9f7
Pandev361 <dev.panajotov@gmail.com>
2025-06-02 16:10:20 +0200

    Projektdateien hinzufügen.

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] food = { "Käse", "Ananas", "Döner", "Reis", "Sahne", "Paprika", "Salami", "Schinken" };
+
+            var sortetArr = SortingArr(food);
+
+            BinarySearching("Kä", sortetArr);
+        }
+
+        private static string BinarySearching(string searchString, string[] sortetArr)
+        {
+            var foundItems = new List<string>();
+
+            var left = 0;
+            var right = sortetArr.Length - 1;
+
+
+            while (left <= right)
+            {
+                var mid = (left + right) / 2;
+
+                var cmp = string.CompareOrdinal(searchString, sortetArr[mid]);
+
+                switch (cmp)
+                {
+                    case 0:
+                        return sortetArr[mid];
+                    case < 0:
+                        right = mid - 1;
+                        break;
+                    default:
+                        left = mid + 1;
+                        break;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private static string[] SortingArr(string[] sortingArr)
+        {
+            var sortedArr = new string[sortingArr.Length];
+
+            for (var i = 0; i < sortingArr.Length; i++)
+            {
+                for (var j = i + 1; j < sortingArr.Length; j++)
+                {
+                    if (string.CompareOrdinal(sortingArr[i], sortingArr[j]) <= 0) continue;
+                    var temp = sortingArr[i];
+                    sortingArr[i] = sortingArr[j];
+                    sortingArr[j] = temp;
+                }
+
+                sortedArr[i] = sortingArr[i];
+            }
+
+
+            return sortedArr;
+        }
+    }
+}
